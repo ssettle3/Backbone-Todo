95add64084867b8660233810b20f341a9c8910e1
Stephen Settle <ssettle3@gmail.com>
2015-02-12 14:20:05 -0500

    Basic HTML, CSS, with BB Model and Collection Set

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,50 @@
-console.log('The Iron Yard Rocks');
+// Todo Model
+var Todo = Backbone.Model.extend({
+
+	initialize: function (){
+		console.log('A new Todo has been created');
+	},
+
+	idAttribute: '_id',
+
+	defaults: {
+
+		name: '',
+		status: 'open',
+		hidden: false,
+	},
+
+	toggleStatus: function (){
+		if (status === 'open'){
+			status === 'closed';
+		} else {
+			status === 'open';
+		};
+	},
+
+	toggleDelete: function(){
+		if (hidden === false){
+			hidden === true;
+		} else {
+			hidden === false;
+		};
+	},
+
+});
+
+// Collection of Todo Items
+var TodoCol = Backbone.Collection.extend ({
+
+	initialize: function (){
+		console.log('Collection Created');
+	},
+
+	model: Todo,
+
+	// url: 
+
+});
+
+
+
+
